c27b9a9d8f1949f869dc067371ff7879bd2c7f71
anant23O02 <anant.sharma@qsstechnosoft.com>
2025-02-14 12:31:39 +0530

    fixed dashboard

--- client/src/assets/Dummydata/userData.ts
+++ client/src/assets/Dummydata/userData.ts
@@ -1,29 +1,33 @@
-import image from '';
+// import image from '../img/socializing';
 import userDataInterface from "./LocationCardInterface";
 
-export default const userCharlie:userDataInterface = {
+export  const userCharlie:userDataInterface = {
     userId:0,
-    profilePic:image,
+    profilePic:'this',
     name: 'Charlie',
     email: 'Charlie@gmail.com',
     phoneNo: 9889286809,
     birthDay: new Date('08-01-1979'),
     interestCards: [0,3,5],
     interests:[],
-    scheduledEvents:[0,1,2,3],
-    // attendedEvents:[0,1,2,3],
-    attendedEventRatings:[
+    scheduledEvents:[1,2,3,4],
+    attendedEvents:[
         {
-            eventId:0,
+            eventId:4,
             rating:4,
         },{
-            eventId:1,
+            eventId:3,
             rating:5,
         },{
             eventId:2,
+            rating:-1,
         },{
             eventId:3,
             rating:2,
+        }
+        ,{
+            eventId:4,
+            rating:3,
         }
     ],
     favortiteEvents:[0,1,2,3,4,0,1,2,3]
