3a5c73b20ca96fb3253574c8b3787b0c98219731
anant23O02 <anant.sharma@qsstechnosoft.com>
2025-02-17 16:18:22 +0530

    started working on edit-profile page

--- client/src/pages/EditProfile.tsx
+++ client/src/pages/EditProfile.tsx
@@ -1,12 +1,35 @@
 import {Navbar} from "../components/Navbar/Navbar";
-
+import {Footer} from "../components/Footer/Footer";
+import {activities} from "../utils/ActivityImages"
 export const EditProfile:React.FC = () => {
     return (
         <> 
         <Navbar/>
         <section className="container">
+        <div className="stylesProfileSection">
+            <div className="stylesprofileHeading">
 
+            </div>
+            <div className="stylesprofileForm">
+                <div className="stylesprofilePicture">
+
+                </div>
+                <div className="stylesform">
+
+
+                </div>
+                    <div className="stylesformImages">
+                        {
+                            activities.map((image,i) => {
+                                return <img src={image} alt="" />
+                            })
+                        }
+                    </div>
+                    
+            </div>
+        </div>
         </section>
+        <Footer/>
         </>
     )
 }
