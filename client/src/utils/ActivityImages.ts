9c75d5379549288a4fa00ff8300784be2ffc3b60
anant23O02 <anant.sharma@qsstechnosoft.com>
2025-02-17 16:18:22 +0530

    started working on edit-profile page

---
+++ client/src/utils/ActivityImages.ts
@@ -0,0 +1,13 @@
+import child from "../assets/img/child.png";
+import chinese from "../assets/img/chinese.png";
+import golf from "../assets/img/golf.png";
+import socializing from "../assets/img/socializing.png";
+import plays from "../assets/img/plays.png";
+import music from "../assets/img/music.png";
+import cooking from "../assets/img/cooking-dining.png";
+import walking from "../assets/img/walking.png";
+import water from "../assets/img/water-sports.png";
+import exploring from "../assets/img/exploring.png";
+
+
+export const activities = [child,chinese,golf,socializing,plays,music,cooking,walking,water,exploring];
