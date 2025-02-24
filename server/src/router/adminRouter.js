cf1855b802953a43931a634d13dea0d0d6aa5c2b
anant23O02 <anant.sharma@qsstechnosoft.com>
2025-02-24 16:07:03 +0530

    created event type page

--- server/src/router/adminRouter.js
+++ server/src/router/adminRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import {addEvents} from "../controller/addEvents.js";
+import {addEventType} from "../controller/addEventType.js"
 import upload from "../middleware/cloudinaryMiddleware.js";
 const Router = express.Router();
 
 Router.post("/add-event",upload.single("images"),addEvents);
-
+Router.post("/add-event-type",addEventType)
 export default Router;
